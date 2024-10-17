dd1542bf362fad8fdb58f253421a2fae4b5c977d
mpatrikik <m.patrik01@gmail.com>
2024-10-17 16:57:29 +0200

    CardView for parts in the dashboard

--- app/src/main/java/com/example/bpt/PartAdapter.java
+++ app/src/main/java/com/example/bpt/PartAdapter.java
@@ -45,3 +45,31 @@ public class PartAdapter extends RecyclerView.Adapter<PartAdapter.ViewHolder> {
         }
     }
 }
+class Part {
+    private String partName;
+    private double totalDistance;
+
+    // Konstruktor
+    public Part(String partName, double totalDistance) {
+        this.partName = partName;
+        this.totalDistance = totalDistance;
+    }
+
+    // Getterek
+    public String getPartName() {
+        return partName;
+    }
+
+    public double getTotalDistance() {
+        return totalDistance;
+    }
+
+    // Szükség esetén beállíthatsz settereket is, ha módosítani kell az értékeket
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public void setTotalDistance(double totalDistance) {
+        this.totalDistance = totalDistance;
+    }
+}
